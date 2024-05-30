8d3afdf902b7bcee07927fc445a41eb7900d00cb
Otavio14 <otaviorovere14@gmail.com>
2024-05-29 21:19:06 -0300

    Correção no Vite para Deploy

--- frontend/src/pages/home/index.jsx
+++ frontend/src/pages/home/index.jsx
@@ -1,7 +1,6 @@
 export const HomePage = () => {
   return (
     <div>
-      <h1>Teste</h1>
       Essa tela vai ser a Home da loja, ou seja, vai ter os produtos para que o
       cliente possa comprar
     </div>
