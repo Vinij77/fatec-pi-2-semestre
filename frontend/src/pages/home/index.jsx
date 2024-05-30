af9cd5506006416021c25531d1cbb0b5cfd3d11a
Otavio14 <otaviorovere14@gmail.com>
2024-05-29 21:08:50 -0300

    Forçando deploy de teste

--- frontend/src/pages/home/index.jsx
+++ frontend/src/pages/home/index.jsx
@@ -1,6 +1,7 @@
 export const HomePage = () => {
   return (
     <div>
+      <h1>Teste</h1>
       Essa tela vai ser a Home da loja, ou seja, vai ter os produtos para que o
       cliente possa comprar
     </div>
