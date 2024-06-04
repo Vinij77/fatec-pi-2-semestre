e362a0c5fd4c61471a64c0788b9b731f1c9bb222
Vinij77 <161947832+Vinij77@users.noreply.github.com>
2024-06-04 20:31:25 -0300

    Update task.validator.js

--- backend/src/validators/task.validator.js
+++ backend/src/validators/task.validator.js
@@ -1,6 +1,17 @@
-import { body } from 'express-validator'
+import { body, param } from 'express-validator'
 
 export const createTaskValidator = [
   body('title').isString().withMessage("Título inválido"),
-  body('userId').isInt().withMessage("ID do usuário inválido"),
+  body('userId').isInt().withMessage("ID do usuário inválido")
+]
+
+export const updateTaskValidator = [
+  param("id").isInt().withMessage("ID inválido"),
+  body("title").isString().withMessage("Título inválido"),
+  body("userId").isString().withMessage("ID do usuário inválido")
+  
+]
+
+export const deleteTaskValidator = [
+  param("id").isInt().withMessage("ID inválido")
 ]
